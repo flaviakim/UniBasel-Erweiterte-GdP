9561ba4fa20b5a9ee3b71817ae572e17036c24ea
Flavia <Flavia@Flavias-MBP.fritz.box>
2016-10-15 12:30:06 +0200

    ImageArrays part a, b, c done

--- Blatt 3/ImageArrays.java
+++ Blatt 3/ImageArrays.java
@@ -13,60 +13,63 @@ public class ImageArrays {
 
 		//redraw to see the image
 		sourceWindow.redraw();
-
-
-		/*  TODO: create an array that is large enough to hold
-                 *       the complete image information
-                 */
-
-
-
-		/*  TODO: store the complete image information in the
-                 *        array you have created.
-                 *  HINT: int red=sourceWindow.getPixelRed(xPosition, yPosition);
-		 *        int green=sourceWindow.getPixelGreen(xPosition, yPosition);
-		 *        int blue=sourceWindow.getPixelBlue(xPosition, yPosition);
-	 	 */
-
-
-
+		
+		// Create an Array with the needed size.
+		int[] imageData = new int[500*500*3];
+		
+		// Fill the Array with RGB Information (first all 250000 Red values, then all Green and then all Blue values).
+		for (int x = 0; x < 500; x++) {
+			for (int y = 0; y < 500; y++) {
+				imageData[0*500*500 + x*500 + y] = sourceWindow.getPixelRed(x, y);
+				imageData[1*500*500 + x*500 + y] = sourceWindow.getPixelGreen(x, y);
+				imageData[2*500*500 + x*500 + y] = sourceWindow.getPixelBlue(x, y);
+				
+			}
+		}
+		
+		
 		//create & open the second windo (to draw your copy into)
-		ImageWindow destinationWindow= new ImageWindow(500,500);
+		ImageWindow destinationWindow = new ImageWindow(500,500);
 		destinationWindow.openWindow("Image rotated by 90 degree",550,0);
+		
+		// Writes the Data rotated by 90° Clockwise.
+		for (int x = 0; x < 500; x++) {
+			for (int y = 0; y < 500; y++) {
+				int xPos = 500 - 1 - y;
+				int yPos = x;				
+				int red   = imageData[0*500*500 + x*500 + y];
+				int green = imageData[1*500*500 + x*500 + y];
+				int blue  = imageData[2*500*500 + x*500 + y];
+				destinationWindow.setPixel(xPos, yPos, red, green, blue);
+			}
+		}
 
-
-
-		/*  TODO: write back your array data into the destinationWindow so that
-                 *        it appears to be rotated 90 degrees.
-                 *  HINT: destinationWindow.setPixel(xPos,yPos,red,green,blue);
-		 */
-
-
-
-                // redraw to see the changed image
-                destinationWindow.redraw();
-
-                // Create another output window
-		ImageWindow destinationWindow2= new ImageWindow(500,500);
+		// redraw to see the changed image
+        destinationWindow.redraw();
+		
+		// Create another output window
+		ImageWindow destinationWindow2 = new ImageWindow(500,500);
 		destinationWindow2.openWindow("Image with permuted color channels",0,550);
 
 
 
-                /*  TODO: call here your function which permutes the color channels
-                 */
+		/*  TODO: call here your function which permutes the color channels
+		 */
 
 
 
-                // redraw to see the changed image
+		// redraw to see the changed image
 		destinationWindow2.redraw();
 	}
 
 
-        /*  TODO: implement here the function to rotate the color channels.
-         *  HINT: think about the arguments you need for the function.
-         */
+	/*  TODO: implement here the function to rotate the color channels.
+	 *  HINT: think about the arguments you need for the function.
+	 */
 
-
+	public static void changeColorChannels(ImageWindow window, int[] data) {
+		
+	}
 
 
 
