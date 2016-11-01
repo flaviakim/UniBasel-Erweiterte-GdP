02ff5ffe53950c5f290a3d12ec69199dac2fb556
Flavia <flaviabrogle@hotmail.com>
2016-11-01 14:05:12 +0100

    Kasse

---
+++ Blatt 6/Kasse.java
@@ -0,0 +1,15 @@
+public class Kasse {
+	
+	public static void main(String[] args) {
+	
+		Kassenbon b = new Kassenbon( new Adresse("Herbstmesse Basel", "Uni Basel", "Petersplatz", "1", "4001", "Basel"));
+		
+		b.add( new Artikel("Marroni",2,5.40));
+		b.add( new Artikel("Magebrot",5,1.10));
+		b.add( new Artikel("Glühwein",2,6));
+		
+		b.print();
+		
+	}
+	
+}
